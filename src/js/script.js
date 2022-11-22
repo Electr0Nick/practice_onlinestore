d3d968ec0f3ed31b74bbd48e86d7bf9a21e18f58
Electr0Nick <kozhakin.maks@gmail.com>
2022-11-22 10:28:26 +0300

    add burger-menu for header

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,12 @@
 import isWebp from './modules/is_webp_for_css.js';
 isWebp();
 
+const menuButton = document.getElementById('menu-btn');
+const menuNav = document.getElementById('menu-nav');
+
+const addActiveClass = () => {
+  menuNav.classList.toggle('active');
+  console.log('сработал');
+}
+
+menuButton.addEventListener('click', addActiveClass);
