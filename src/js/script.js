532e3ab48a42728c781ae3a648c629fc23f29c63
Electr0Nick <kozhakin.maks@gmail.com>
2022-11-25 09:19:53 +0300

    add scss and js for hide catalog bar

--- src/js/script.js
+++ src/js/script.js
@@ -7,9 +7,30 @@ isWebp();
 import { useDynamicAdapt } from './modules/dynamicAdapt.js'
 useDynamicAdapt();
 
+
 const activeClass = (el) => {
   el.classList.toggle('active');
 }
+
+
+// открытие каталог бара
+const сatalogButton = document.getElementById('ctlg-btn');
+const catalogBody = document.getElementById('ctlg-body');
+const addActiveForCtlg = () => {
+  activeClass(catalogBody);
+}
+сatalogButton.addEventListener('click', addActiveForCtlg);
+
+
+// открытие подменю каталог бара
+const tempItem = document.getElementById('temp-item');
+const tempList = document.getElementById('temp-list');
+const addActiveForTemp = () => {
+  activeClass(tempList);
+  activeClass(tempItem);
+}
+tempItem.addEventListener('click', addActiveForTemp);
+
 
 // бургер меню
 const menuButton = document.getElementById('menu-btn');
