c4c25cd41cb3117bf70805060b2dfe816f62edde
Electr0Nick <kozhakin.maks@gmail.com>
2022-11-26 09:17:47 +0300

    add JS for catalogBar (I think it's not correct)

--- src/js/script.js
+++ src/js/script.js
@@ -12,26 +12,6 @@ const activeClass = (el) => {
   el.classList.toggle('active');
 }
 
-
-// открытие каталог бара
-const сatalogButton = document.getElementById('ctlg-btn');
-const catalogBody = document.getElementById('ctlg-body');
-const addActiveForCtlg = () => {
-  activeClass(catalogBody);
-}
-сatalogButton.addEventListener('click', addActiveForCtlg);
-
-
-// открытие подменю каталог бара
-const tempItem = document.getElementById('temp-item');
-const tempList = document.getElementById('temp-list');
-const addActiveForTemp = () => {
-  activeClass(tempList);
-  activeClass(tempItem);
-}
-tempItem.addEventListener('click', addActiveForTemp);
-
-
 // бургер меню
 const menuButton = document.getElementById('menu-btn');
 const menuNav = document.getElementById('menu-nav');
@@ -40,3 +20,47 @@ const addActiveForNav = () => {
   activeClass(menuButton);
 }
 menuButton.addEventListener('click', addActiveForNav);
+
+
+// открытие каталог бара
+const catalogBar = document.getElementById('ctlg');
+const catalogHead = document.getElementById('ctlg-head');
+const сatalogButton = document.getElementById('ctlg-btn');
+const arrCatalogBarItems = document.querySelectorAll('.catalogbar__list_main > .catalogbar__item');
+const catalogHeadStyles = getComputedStyle(catalogHead);
+const catalogBarItemStyles = getComputedStyle(arrCatalogBarItems[0]);
+const catalogBarItemHeight = catalogBarItemStyles.height;
+const openCatalogBarHeight = parseInt(catalogHeadStyles.height) + arrCatalogBarItems.length * parseInt(catalogBarItemHeight);
+const closeCatalogBarHeight = parseInt(catalogHeadStyles.height);
+
+const arrCatalogBarLinksMenu = document.querySelectorAll('.catalogbar__link_menu');
+
+const addActiveForCtlgLinks = (event) => {
+  for (let i = 0; i < arrCatalogBarLinksMenu.length; i++) {
+    arrCatalogBarLinksMenu[i].nextElementSibling.classList.remove('active');
+  }
+  const eventLink = event.target;
+  const hideList = eventLink.nextElementSibling;
+  const hideListElements = eventLink.nextElementSibling.children;
+  const hideListHeight = hideListElements.length * parseInt(catalogBarItemHeight);
+  activeClass(eventLink);
+  activeClass(hideList);
+  if (hideList.classList.contains('active')) {
+    catalogBar.style.height = `${openCatalogBarHeight + hideListHeight}px`;
+  } else {
+    catalogBar.style.height = `${openCatalogBarHeight}px`;
+  }
+}
+for (let i = 0; i < arrCatalogBarLinksMenu.length; i++) {
+  arrCatalogBarLinksMenu[i].addEventListener('click', addActiveForCtlgLinks);
+}
+
+const addActiveForCtlg = () => {
+    activeClass(catalogBar);
+    if (catalogBar.classList.contains('active')) {
+      catalogBar.style.height = `${openCatalogBarHeight}px`;
+    } else {
+      catalogBar.style.height = `${closeCatalogBarHeight}px`;
+    }
+  }
+сatalogButton.addEventListener('click', addActiveForCtlg);
